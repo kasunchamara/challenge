4e74ebf608950cb0cc726e53bcca7743ff13af2c
Kasun_107110 <Kasun.Chamara@axiatadigitallabs.com>
2021-03-31 07:58:03 +0530

    Add comment

--- app/src/main/java/com/mobile/otrium/di/auth/AuthInterceptor.kt
+++ app/src/main/java/com/mobile/otrium/di/auth/AuthInterceptor.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import okhttp3.Interceptor
 import okhttp3.Response
 
+/*
+*  authentication interceptor
+*  add token
+* */
 class AuthInterceptor(val context: Context) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer 80816d7ad52ff00e78222832f84251c555b9ab93")
+            .addHeader("Authorization", "Bearer f58815d510d5619d5b5030852cc0d2bdb8a7d8af")
             .build()
 
         return chain.proceed(request)
